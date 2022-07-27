a3b9fb59fa8db0f8e11f3be331084b686f09edcd
Kawsar Ahmed Fahad <kafaha1999@gmail.com>
2022-07-28 02:08:13 +0600

    innitial stage complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import { NavbarMenu } from './components/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Sidebar } from './components/Sidebar';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
+import { Contact } from './pages/Contact';
 
 function App() {
   return (
     <>
       {/* <NavbarMenu /> */}
-      <Sidebar />
+      <BrowserRouter>
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Sidebar>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,59 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { NavLink } from 'react-router-dom';
 
-export const Sidebar = () => {
-  return <div>Sidebar</div>;
+// icons
+import { FaBars, FaHome, FaUser } from 'react-icons/fa';
+import { MdMessage } from 'react-icons/md';
+import { BiSearch } from 'react-icons/bi';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Dashboard',
+    icon: <FaHome />,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    icon: <FaUser />,
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    icon: <MdMessage />,
+  },
+];
+
+export const Sidebar = ({ children }) => {
+  return (
+    <div className="mainContainer">
+      <motion.div animate={{ width: '200px' }} className="sidebar">
+        <div className="top-section">
+          <h1 className="logo">faha1999</h1>
+
+          <div className="bars">
+            <FaBars />
+          </div>
+        </div>
+
+        <div className="search">
+          <div className="search-icon">
+            <BiSearch />
+          </div>
+          <input placeholder="Search..." />
+        </div>
+
+        <section className="routes">
+          {routes.map((route) => (
+            <NavLink to={route.path} key={route.name} className="link">
+              <div className="icon">{route.icon}</div>
+              <div className="title">{route.name}</div>
+            </NavLink>
+          ))}
+        </section>
+      </motion.div>
+      <main>{children}</main>
+    </div>
+  );
 };
